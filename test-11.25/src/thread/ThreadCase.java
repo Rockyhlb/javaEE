ebcea239228cfb285d6dd4ee08a51052e66e6330
codehlb <2396018503@qq.com>
2023-11-26 20:11:57 +0800

    test-11.25 内存可见性 单例模式 wait notify

--- test-11.25/src/thread/ThreadCase.java
+++ test-11.25/src/thread/ThreadCase.java
@@ -1,7 +1,5 @@
 package thread;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
-
 /**
  * @BelongsProject: test-11.25
  * @BelongsPackage: thread
@@ -27,7 +25,7 @@ public class ThreadCase {
  * 如果多个线程，同时修改同一个变量，此时就可能出现线程安全问题
  * 饿汉模式(线程安全)：线程调用 getInstance() 只是读取操作，并不会修改变量
  * 懒汉模式(线程不安全)：线程调用 getInstance() 涉及读取和修改操作，因此可能会发生线程不安全问题，也就违背了单例的要求，
- * 可以通过加锁操作解决懒汉模式的线程不安全问题，但是加锁操作也就导致了这个代码基本上 不可能是 ”高性能“ 代码；
+ * 可以通过加锁操作解决懒汉模式的线程不安全问题，但是加锁操作也就导致了这个代码基本上 不可能是 ”高性能“ 代码;
  */
 class Singleton {
     // 1、声明一个静态的实例对象
@@ -38,17 +36,25 @@ class Singleton {
     }
     // 3、提供一个公共方法获取当前的实例
     public static Singleton getInstance() {
+        // 懒汉模式，非必要不创建，节省内存开销从而提高了效率，但同时也带来了线程安全问题
         if (instance == null) {
             instance = new Singleton();
         }
         return instance;
     }
-
     public static Singleton getInstance1() {
         // 虽然这样解决了线程不安全问题，却也导致了每次调用方法时都会进行加锁操作，但是我们的单例模式其实只要第一次加锁就行了
-        synchronized (Singleton.class) {
-            if (instance == null) {
-                instance = new Singleton();
+//        synchronized (Singleton.class) {
+//            if (instance == null) {
+//                instance = new Singleton();
+//            }
+//        }
+        // 外面再加一个判定条件，当实例对象为空时，再进行加锁操作保证线程安全。当对象已经有了，那线程就是安全的，就不需要加锁了
+        if(instance != null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
             }
         }
         return instance;
