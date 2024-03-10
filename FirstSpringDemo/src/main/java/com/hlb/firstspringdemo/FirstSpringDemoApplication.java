cfb8ce7e2aeafe81bdeb0e3b24db36a40753d2d8
codehlb <2396018503@qq.com>
2024-03-10 22:03:27 +0800

    MVC SpringMVC RequestMapping RequestParam

---
+++ FirstSpringDemo/src/main/java/com/hlb/firstspringdemo/FirstSpringDemoApplication.java
@@ -0,0 +1,11 @@
+package com.hlb.firstspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringDemoApplication.class, args);
+    }
+}
